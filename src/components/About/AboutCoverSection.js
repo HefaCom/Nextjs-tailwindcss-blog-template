52da379f6157a79c2eb36f91b11c308d208905e5
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2024-01-27 19:24:13 +0300

    Update AboutCoverSection.js

--- src/components/About/AboutCoverSection.js
+++ src/components/About/AboutCoverSection.js
@@ -17,15 +17,13 @@ const AboutCoverSection = () => {
 
         <div className='w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16'>
             <h2 className='font-bold capitalize text-4xl xs:text-5xl sxl:text-6xl  text-center lg:text-left'>
-            Dream Big, Think Differently, Work Hard, Achieve More!
+            Dream Big, Grind, Achieve More!
             </h2>
             <p className='font-medium capitalize mt-4 text-base'>
-            I am a Freelance Data Analyst and a programmer, specializing in transforming raw data into meaningful insights. With a passion for data and programming, I have successfully completed multiple projects for clients across various industries and domains, delivering high-quality and accurate work in data analysis and interpretation, predictive modeling, data visualization, web development, and GUI programming.<br/>
+            I am a Freelance Data Analyst and a programmer, specializing in transforming raw data into meaningful insights. With a passion for data and programming, I have successfully completed multiple projects for clients across various industries and domains, delivering high-quality and accurate work in data analysis and interpretation, predictive modeling, data visualization, web development, and GUI programming. Having Experience with:R, Python, JMP, SPSS, Excel, React, PHP, Wix, WordPress, Java, C#, C++, and VB.<br/>
               
             </p>
-  <p className='font-medium capitalize mt-4 text-base'>
-              I have over 6 years of experience in using a range of tools and languages, such as R, Python, JMP, SPSS, Excel, React, PHP, Wix, WordPress, Java, C#, C++, and VB, to handle complex datasets, build predictive models, create visually appealing data representations, and develop user-friendly web applications and interfaces. Currently, I am enhancing my skills and knowledge in data science by pursuing a Master's degree at Moringa School, one of the leading institutions in this fast-growing field. My goal is to leverage my expertise to drive innovation and provide tangible value to businesses.
-               </p>
+
         </div>
     </section>
   )
