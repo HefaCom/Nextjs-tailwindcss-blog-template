35e78ea113105dfa35398791d6c5ffb78f17d074
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2024-01-27 22:41:45 +0300

    Update siteMetaData.js

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -6,7 +6,7 @@ const siteMetadata = {
     description: 'LearnMentorHub is a dynamic educational platform committed to fostering learning and mentorship. .',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://blog-template-sigma-hazel.vercel.app', // your website URL
+    siteUrl: 'https://learn.nerdexperts.online/', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
     email: 'okokohhezron254@gmail.com', 
