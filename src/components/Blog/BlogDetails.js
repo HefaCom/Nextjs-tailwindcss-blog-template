8167813c486309c5d570a21c8bd9fc50696b19f5
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2024-01-27 18:57:57 +0300

    Update BlogDetails.js

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 import { slug } from "github-slugger";
 import ViewCounter from "./ViewCounter";
-
+const author  = "Hezron"
 const BlogDetails = ({ blog, slug: blogSlug }) => {
   return (
     <div className="px-2  md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
@@ -13,6 +13,7 @@ const BlogDetails = ({ blog, slug: blogSlug }) => {
       <span className="m-3">
         <ViewCounter slug={blogSlug} />
       </span>
+    <div className="m-3">{author}</div>
       <div className="m-3">{blog.readingTime.text}</div>
       <Link href={`/categories/${slug(blog.tags[0])}`} className="m-3">
         #{blog.tags[0]}
