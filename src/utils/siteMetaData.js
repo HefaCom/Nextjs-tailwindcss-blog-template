387a69ac907a7d43e5347d678e470de5307807ae
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2024-01-27 19:21:03 +0300

    Update siteMetaData.js

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,19 +1,19 @@
 // please update required information
 const siteMetadata = {
     title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-    author: 'CodeBucks',
-    headerTitle: 'Next.js Blog',
-    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    author: 'Hezron Okoko',
+    headerTitle: 'Learn it LearnMentorHub',
+    description: 'LearnMentorHub is a dynamic educational platform committed to fostering learning and mentorship. .',
     language: 'en-us',
     theme: 'system', // system, dark or light
-    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteUrl: 'https://blog-template-sigma-hazel.vercel.app', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
     email: 'okokohhezron254@gmail.com', 
     github: 'https://github.com/HefaCom',
     twitter: 'https://twitter.com/_hezron__',
     facebook: 'https://facebook.com',
-    youtube: 'https://youtube.com/codebucks',
+    youtube: 'https://youtube.com/hezroncreations',
     linkedin: 'https://www.linkedin.com/in/hezron-okoko/',
     dribbble: 'https://www.dribbble.com',
     locale: 'en-US',
