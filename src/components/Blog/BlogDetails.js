be0caea1ec35532be62e640183f92d555401b4ed
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2024-01-27 19:02:22 +0300

    Update BlogDetails.js

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 import { slug } from "github-slugger";
 import ViewCounter from "./ViewCounter";
-const author  = "Hezron"
+const author  = "by: Hezron Okoko"
 const BlogDetails = ({ blog, slug: blogSlug }) => {
   return (
     <div className="px-2  md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5  md:mx-10 rounded-lg">
