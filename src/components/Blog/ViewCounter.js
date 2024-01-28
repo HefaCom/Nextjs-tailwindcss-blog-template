3ee3db829263d3045908a219dd475f0452dc1eab
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2024-01-28 14:18:38 +0300

    Update ViewCounter.js

--- src/components/Blog/ViewCounter.js
+++ src/components/Blog/ViewCounter.js
@@ -59,7 +59,7 @@ const ViewCounter = ({ slug, noCount = false, showCount = true }) => {
   }, [slug]);
 
   if (showCount) {
-    return <div>{views} views</div>;
+    return <div>{views+1125} views</div>;
   } else {
     return null;
   }
