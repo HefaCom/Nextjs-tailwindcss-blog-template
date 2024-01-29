e9db769e4a0175d61d198a1438b4d5951a1c4a8e
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2024-01-29 11:08:14 +0300

    Update Logo.js

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
-import profileImg from "@/public/logo.png"
+import profileImg from "@/public/logol.png"
 
 const Logo = () => {
   return (
