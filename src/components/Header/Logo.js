0f7a00d6983770dab9cd3382d248e821536681cc
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2024-01-29 11:48:09 +0300

    Update Logo.js

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
         <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
-            <Image src={profileImg} alt="LearnMentorHub logo" className="h-1/2 w-1/2 rounded-full" sizes="20vw" priority />
+            <Image src={profileImg} alt="LearnMentorHub logo" className="h-1/2 w-full rounded-full" sizes="20vw" priority />
         </div>
         <span className="font-bold dark:font-semibold text-lg md:text-xl">LearnMentorHub</span>
     </Link>
